93a0f8d6f01ae2cd1ade7f2c44f63fd46b283553
Chris Fedde <cfedde@cpan.org>
2006-07-06 16:42:05 +0000

    Import of CFEDDE/POE-Session-AttributeBased-0.04 from CPAN.
    gitpan-cpan-distribution: POE-Session-AttributeBased
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         CFEDDE/POE-Session-AttributeBased-0.04.tar.gz
    gitpan-cpan-author:       CFEDDE
    gitpan-cpan-maturity:     released

--- lib/POE/Session/AttributeBased.pm
+++ lib/POE/Session/AttributeBased.pm
@@ -12,11 +12,11 @@ POE::Session::AttributeBased - POE::Session using attributes marking state handl
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
